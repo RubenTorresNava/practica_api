4a3ec500eae5dd5117b2de9e8e741808ea942eee
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-02-08 10:41:44 -0600

    login y logout

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -53,4 +53,44 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send('Usuario login');
+export const login = async (req, res) => {
+  try {
+    console.log(req.body);
+    // Obtener los datos del usuario
+    const { username, password } = req.body;
+
+    // Verificar si el usuario existe
+    const existing = await User.findOne({ username });
+    console.log(existing);
+    if (!existing) {
+      return res.status(400).json({ message: 'El usuario no existe' });
+    }
+    //devolver el token e caso de que exista
+    jwt.sign(
+      { id: existing._id, phone: existing.phone, email: existing.email },
+      'utd123',
+      { expiresIn: '1d' },
+      (error, token) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ message: 'Error interno del servidor' });
+        } else {
+          // Responder con el token
+          res.json({ token });
+        }
+      }
+    );
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
+//log out cerrar sesion y eliminar token
+export const logout = async (req, res) => {
+  try {
+    res.json({ token: '' });
+  } catch (error) {
+    console.error('Error al cerrar sesión:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { login, register } from "../controller/auth.controller.js";
+import { login, logout, register } from "../controller/auth.controller.js";
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.post("/logout", logout)
 export default router;
