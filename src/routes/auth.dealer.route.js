b95aa4739a612d117a294f9498bf61d6ee122ffc
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-02-11 14:45:57 -0600

    nuevo campo de fechacreacion

--- src/routes/auth.dealer.route.js
+++ src/routes/auth.dealer.route.js
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { getDealers, login, register, update } from '../controller/auth.dealer.controller.js';
+import { getDealers, login, register, update, addFechaCreacion } from '../controller/auth.dealer.controller.js';
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/dealers", getDealers)
 router.patch("/update", update);
+router.put("/addFechaCreacion", addFechaCreacion);
 export default router;
