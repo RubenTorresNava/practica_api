bc744ea1ca26824dfc19db88353a3044ed47c266
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-02-11 14:39:45 -0600

    actualizacion de telefono y correo de dealers

--- src/routes/auth.dealer.route.js
+++ src/routes/auth.dealer.route.js
@@ -1,8 +1,9 @@
 import {Router} from 'express';
-import { getDealers, login, register } from '../controller/auth.dealer.controller.js';
+import { getDealers, login, register, update } from '../controller/auth.dealer.controller.js';
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/dealers", getDealers)
+router.patch("/update", update);
 export default router;
