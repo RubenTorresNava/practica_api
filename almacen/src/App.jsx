8dddf1f8ba0ff8b21f85fe313c0d1687a35a5f72
Ruben Torres <rubenbebe.145.rt@gmail.com>
2024-02-22 10:36:51 -0600

    Agregado de ruta de inicio y estilos de navegación

--- almacen/src/App.jsx
+++ almacen/src/App.jsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Loginpage from './pages/Loginpage';
 import Registerpage from './pages/Registerpage';
+import Home from './pages/Home';
 function app() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='"/" 'element={<h1>Home</h1>} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Loginpage />} />
             <Route path="/register" element={<Registerpage />} />
         </Routes>
---
+++ almacen/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {  
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <Link className="navegar" to="/login">Iniciar sesion</Link>
+      <Link className="navegar" to="/register">Registrar un Usuario</Link>
+    </div>
+  );
+}
+export default Home;
